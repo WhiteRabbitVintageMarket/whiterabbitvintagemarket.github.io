8e69dc1f14761cfa489cbba0d69bab081503d163
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2024-01-01 23:57:50 -0600

    Add subtotal to shopping cart page (#9)

--- public/js/cart.mjs
+++ public/js/cart.mjs
@@ -1,4 +1,4 @@
-import { formatPrice } from "/js/money.mjs";
+import { formatPrice, calculateTotal } from "/js/money.mjs";
 
 const localStorageKey = "shoppingCart";
 
@@ -27,10 +27,31 @@ export function isProductInCart(productId) {
   return Boolean(result);
 }
 
-function removeProductFromCart() {}
+function removeProductFromCart(productId) {
+  const { products } = getCartLocalStorage();
+
+  const productsToKeep = products.filter((product) => {
+    return product.id !== productId;
+  });
+
+  setCartLocalStorage({ products: productsToKeep });
+  updateCartProductCount();
+}
 
 export function displayCart({ allProducts, container }) {
+  const productList = document.querySelector("#product-list");
+  const cartSummary = document.querySelector("#cart-summary");
+
+  if (productList) {
+    productList.remove();
+  }
+
+  if (cartSummary) {
+    cartSummary.remove();
+  }
+
   const { products: cartProducts } = getCartLocalStorage();
+
   if (cartProducts.length === 0) {
     return container.querySelector("#empty-cart").classList.remove("hidden");
   }
@@ -41,35 +62,97 @@ export function displayCart({ allProducts, container }) {
   });
 
   const unorderedList = document.createElement("ul");
+  unorderedList.id = "product-list";
+
   for (const selectedProduct of selectedProducts) {
-    const listItem = document.createElement("li");
-    listItem.classList.add("flex", "justify-start", "h-32", "my-4");
-
-    const productImage = document.createElement("img");
-    productImage.classList.add(
-      "object-cover",
-      "aspect-square",
-      "bg-stone-300",
-      "mr-4",
-    );
-    productImage.src = selectedProduct.url;
-
-    const productName = document.createElement("p");
-    productName.innerText = selectedProduct.name;
-    productName.classList.add("grow");
-
-    const productPrice = document.createElement("div");
-    productPrice.innerText = formatPrice(selectedProduct.price);
-    productPrice.classList.add("w-40", "flex", "justify-end", "ml-4");
-
-    listItem.appendChild(productImage);
-    listItem.appendChild(productName);
-    listItem.appendChild(productPrice);
-
-    unorderedList.appendChild(listItem);
+    const { removeProductButton } = displayProduct({
+      product: selectedProduct,
+      container: unorderedList,
+    });
+    removeProductButton.onclick = (event) => {
+      removeProductFromCart(selectedProduct.id);
+      displayCart({ allProducts, container });
+    };
   }
 
   container.appendChild(unorderedList);
+
+  const summaryRow = document.createElement("div");
+  summaryRow.id = "cart-summary";
+  summaryRow.classList.add("flex", "justify-between", "sm:justify-end", "ml-4");
+
+  const label = document.createElement("p");
+  label.classList.add("sm:mr-32");
+  label.innerText = "Subtotal";
+
+  const amount = document.createElement("p");
+  const subTotal = calculateTotal(selectedProducts);
+  amount.innerText = formatPrice(subTotal);
+
+  summaryRow.appendChild(label);
+  summaryRow.appendChild(amount);
+
+  container.appendChild(summaryRow);
+}
+
+function displayProduct({ product, container }) {
+  const listItem = document.createElement("li");
+  listItem.id = `product-${product.id}`;
+  listItem.classList.add(
+    "flex",
+    "flex-wrap",
+    "justify-start",
+    "items-start",
+    "min-h-32",
+    "my-4",
+    "pb-4",
+    "border-b",
+    "border-stone-300",
+  );
+
+  const productImage = document.createElement("img");
+  productImage.classList.add(
+    "w-32",
+    "object-cover",
+    "aspect-square",
+    "bg-stone-300",
+    "mr-4",
+  );
+  productImage.src = product.url;
+
+  const productName = document.createElement("p");
+  productName.innerText = product.name;
+  productName.classList.add("grow", "basis-1/5");
+
+  const productPrice = document.createElement("div");
+  productPrice.classList.add(
+    "w-full",
+    "sm:w-40",
+    "order-2",
+    "sm:order-none",
+    "flex",
+    "justify-end",
+    "ml-4",
+  );
+  productPrice.innerText = formatPrice(product.price);
+
+  const removeProductButton = document.createElement("button");
+  removeProductButton.classList.add("ml-6");
+  removeProductButton.innerHTML = `
+    <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 18 18">
+      <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+    </svg>`;
+
+  listItem.appendChild(productImage);
+  listItem.appendChild(productName);
+  listItem.appendChild(productPrice);
+  listItem.appendChild(removeProductButton);
+
+  container.appendChild(listItem);
+
+  return {
+    removeProductButton,
+  };
 }
 
 function getCartLocalStorage() {
--- public/js/money.mjs
+++ public/js/money.mjs
@@ -6,3 +6,18 @@ export function formatPrice(price) {
 
   return formattedPrice;
 }
+
+export function roundTwoDecimals(value) {
+  return Math.round((value + Number.EPSILON) * 100) / 100;
+}
+
+export function calculateTotal(productsArray) {
+  console.log(productsArray);
+  const total = productsArray.reduce((partialSum, product) => {
+    const quantity = product.quantity ?? 1;
+    const newTotal =
+      partialSum + parseFloat(product.price) * parseInt(quantity);
+    return newTotal;
+  }, 0);
+  return roundTwoDecimals(total);
+}
